1a5d1129eff781a18900f3c7e43829a3a7356397
Luciano Amâncio <madruga665@hotmail.com>
2021-10-26 05:22:14 -0300

    refatoração da função fetch

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,51 +1,40 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
+import 'package:newapp/models/pokemon.dart';
 
 Future fetch() async {
   String url =
       "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
   final response = await http.get(Uri.parse(url));
   final decoded = jsonDecode(response.body)['pokemon'];
-  final result = decoded.map((data) =>
-      Pokemon(id: data.id, name: data.name, num: data.num, img: data.img));
+  final result = decoded
+      .map((data) => Pokemon(
+          id: data['id'],
+          name: data['name'],
+          num: data['num'],
+          img: data['img']))
+      .toList();
   print(result[0]);
   return result;
 }
 
-class Pokemon {
-  int id;
-  String num;
-  String name;
-  String img;
 
-  Pokemon({
-    required this.id,
-    required this.num,
-    required this.name,
-    required this.img,
-  });
 
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'num': num,
-      'name': name,
-      'img': img,
-    };
-  }
+// Outra forma de fazer o fetch
+// Future fetch() async {
+//   String url =
+//       "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+//   final response = await http.get(Uri.parse(url));
+//   final decoded = jsonDecode(response.body)['pokemon'];
+//   final List<Pokemon> pokemons = [];
 
-  factory Pokemon.fromMap(Map<String, dynamic> map) {
-    return Pokemon(
-      id: map['id'],
-      num: map['num'],
-      name: map['name'],
-      img: map['img'],
-    );
-  }
-
-  String toJson() => json.encode(toMap());
-
-  factory Pokemon.fromJson(String source) =>
-      Pokemon.fromMap(json.decode(source));
-}
+//   for (var item in decoded) {
+//     Pokemon pokemon = Pokemon(
+//         id: item['id'],
+//         num: item['num'],
+//         name: item['name'],
+//         img: item['img']);
+//     pokemons.add(pokemon);
+//   }
+//   return pokemons;
+// }
