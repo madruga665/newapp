d3326b846c43262e5d51163b574e646f250910ec
Luciano Amâncio <madruga665@hotmail.com>
2021-11-26 10:25:59 -0300

    chore: remove comentários da api request

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -14,27 +14,5 @@ Future fetch() async {
           num: data['num'],
           img: data['img']))
       .toList();
-  print(result[0]);
   return result;
 }
-
-
-
-// Outra forma de fazer o fetch
-// Future fetch() async {
-//   String url =
-//       "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
-//   final response = await http.get(Uri.parse(url));
-//   final decoded = jsonDecode(response.body)['pokemon'];
-//   final List<Pokemon> pokemons = [];
-
-//   for (var item in decoded) {
-//     Pokemon pokemon = Pokemon(
-//         id: item['id'],
-//         num: item['num'],
-//         name: item['name'],
-//         img: item['img']);
-//     pokemons.add(pokemon);
-//   }
-//   return pokemons;
-// }
