1f7ba5db4f80ef5e42d0dbef5856aaabc5667b63
Luciano Amâncio <madruga665@hotmail.com>
2023-01-21 08:25:27 -0300

    add todo repository

---
+++ lib/repositories/todo_repository.dart
@@ -0,0 +1,15 @@
+import 'package:dio/dio.dart';
+import 'package:newapp/models/todo_model.dart';
+
+class TodoRepository {
+  final dio = Dio();
+
+  Future<List<TodoModel>> fetchTodos() async {
+    final response =
+        await dio.get('https://jsonplaceholder.typicode.com/todos');
+    final todosList = response.data as List;
+    final todos = todosList.map((todo) => TodoModel.fromMap(todo)).toList();
+
+    return todos;
+  }
+}
