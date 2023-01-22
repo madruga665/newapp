f5b127255e856657d5fc7975bc07b27590f26aa9
Luciano Amâncio <madruga665@hotmail.com>
2023-01-22 00:39:50 -0300

    lista as tarefas na pagina

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:newapp/models/todo_model.dart';
+import 'package:newapp/repositories/todo_repository.dart';
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({Key? key}) : super(key: key);
@@ -8,11 +10,35 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  final todoRepository = TodoRepository();
+  List<TodoModel> todos = [];
+
+  getAllTodos() async {
+    todos = await todoRepository.fetchTodos();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getAllTodos();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Container(
-      child: Text('Aqui vem as tasks'),
-    ));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Lista de Tarefas'),
+      ),
+      body: ListView.builder(
+        itemCount: todos.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(todos[index].title),
+            tileColor: Colors.blue[700],
+            textColor: Colors.grey[300],
+          );
+        },
+      ),
+    );
   }
 }
