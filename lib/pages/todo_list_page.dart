1e6ba80f395dfd7da3b7385d00737774d4902211
Luciano Amâncio <madruga665@hotmail.com>
2023-01-22 09:07:07 -0300

    add estilo para lista de tarefas

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -30,19 +30,41 @@ class _TodoListPageState extends State<TodoListPage> {
       body: AnimatedBuilder(
         animation: todoListController,
         builder: (context, child) {
-          return ListView.builder(
-            itemCount: todoListController.todos.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListTile(
-                  title: Text(todoListController.todos[index].title),
-                  tileColor: Colors.blue[700],
-                  textColor: Colors.grey[300],
-                ),
-              );
-            },
-          );
+          return todoListController.todos.length > 0
+              ? ListView.builder(
+                  itemCount: todoListController.todos.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListTile(
+                        tileColor: Colors.blue[500],
+                        textColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        leading: Container(
+                          padding: const EdgeInsets.only(right: 12),
+                          decoration: const BoxDecoration(
+                            border: Border(
+                              right: BorderSide(width: 1, color: Colors.white),
+                            ),
+                          ),
+                          child: CircleAvatar(
+                            child: Text(
+                              todoListController.todos[index].id.toString(),
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        ),
+                        title: Text(todoListController.todos[index].title),
+                        trailing: Checkbox(
+                          onChanged: ((value) {}),
+                          value: todoListController.todos[index].completed,
+                        ),
+                      ),
+                    );
+                  },
+                )
+              : Center(child: CircularProgressIndicator());
         },
       ),
     );
